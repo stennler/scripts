e8d71be89223853ceaf6cc6375ded475576a53f5
Zack Fleischman <zFleischman@counsyl.com>
2016-01-12 12:48:09 -0800

    [Backup Script]

--- python/zutil.py
+++ python/zutil.py
@@ -14,6 +14,19 @@ def import_all(import_dict):
             import_dict[name] = func
 
 
+def zlist():
+    """Lists all the zutil functions"""
+    members = inspect.getmembers(zutil, inspect.isfunction)
+    members.sort()
+    for name, func in members:
+        print("-"*(1+len(name)))
+        print("%s:" % name)
+        print("-"*(1+len(name)))
+        print(func.__doc__)
+        print("")
+        print("")
+
+
 def h(max_history=25):
     """Prints the history up to a certain max (max is optional)"""
     zHistory.h(max_history)
@@ -22,6 +35,6 @@ def h(max_history=25):
 def explore(package_name, show_classes_and_functions=False, offset=""):
     """Recursively lists modules in a package.
 
-       If True is passed as 2nd arg it will list classes and functions too.
-       """
+If True is passed as 2nd arg it will list classes and functions too.
+"""
     zExplore.explore(package_name, show_classes_and_functions, offset)
