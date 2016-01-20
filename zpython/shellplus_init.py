6d02b0c5e8de4a3dca155a94348cabd4da12e1d0
Zack Fleischman <zFleischman@counsyl.com>
2016-01-19 22:21:01 -0800

    deleting file ' :

--- zpython/shellplus_init.py
+++
@@ -1,5 +0,0 @@
-from counsyl.product.my.utils.fake import fake_profile
-print ("imported: fake_profile")
-
-from counsyl.product.common import enumerated_types as G
-print ("imported: G [G.GENDER_MALE, G.GENDER_FEMALE]")
